7a42cdb72f75260be14e709db14bcf5d52e41562
Marcos Rogerio <marcos.pinheiro@fbter.org.br>
2020-08-08 12:14:26 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ComputerVision
 Basic codes of point cloud (pcl) with kinect
+
+create "projectX" folder
+create "file.cpp"
+create "build" folder inside "projectX"
+create CMakeLists.txt
+inside of "build" excute "cmake .."
+after this, execute "make"
+after this, excute ./file
+
---
+++ pasta02/build/CMakeFiles/openni_grabber.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openni_grabber.dir/openni_grabber.cpp.o"
+  "openni_grabber"
+  "openni_grabber.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/openni_grabber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
